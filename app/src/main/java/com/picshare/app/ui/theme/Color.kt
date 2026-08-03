a54250e6fbaeb12ac6eb9c75948ea267113dc904
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-08-03 18:51:49 +0200

    remove: light theme data

--- app/src/main/java/com/picshare/app/ui/theme/Color.kt
+++ app/src/main/java/com/picshare/app/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.picshare.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val AppBackground = Color(0xFF121316)
+val CardBackground1 = Color(0xFF262a33)
+val CardBackground2 = Color(0xFF2d313c)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
