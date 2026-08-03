e1361e1e2d4c70bc9c058b285bdc4dddd6dd6db9
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-08-03 18:50:32 +0200

    add: login activity

---
+++ app/src/main/java/com/picshare/app/LoginActivity.kt
@@ -0,0 +1,99 @@
+package com.picshare.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.picshare.app.ui.theme.PicshareTheme
+
+class LoginActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      PicshareTheme {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+          LoginScreen()
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun LoginScreen() {
+  Box(
+    modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.Center
+  ){
+    LoginCard()
+
+  }
+}
+
+@Composable
+fun LoginCard() {
+  Column(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(24.dp),
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Image(
+      painter = painterResource(id = R.drawable.logo),
+      contentDescription = ""
+    )
+    Spacer(Modifier.height(48.dp))
+
+    LoginButtons()
+  }
+}
+
+@Composable
+fun LoginButtons(){
+  Card(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(24.dp),
+    shape = RoundedCornerShape(16.dp)
+  ) {
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier.padding(32.dp)
+    ) {
+      Text( text = "Get started by signing in to your account")
+      Spacer(Modifier.height(24.dp))
+    }
+  }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun loginScreenPreview() {
+  PicshareTheme {
+    LoginScreen()
+  }
+}
