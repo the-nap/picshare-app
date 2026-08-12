c640f27621016d45dda81d99d8e28589dcd0bb95
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-08-12 17:18:39 +0200

    feat: add auth interceptors

--- app/src/main/java/com/picshare/app/di/AppModule.kt
+++ app/src/main/java/com/picshare/app/di/AppModule.kt
@@ -2,11 +2,13 @@ package com.picshare.app.di
 
 import com.picshare.app.BuildConfig
 import com.picshare.app.api.network.PicshareApi
+import com.picshare.app.auth.AuthInterceptor
 import com.picshare.app.post.PostRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -23,6 +25,13 @@ object AppModule {
       .addConverterFactory(GsonConverterFactory.create())
       .build()
       .create(PicshareApi::class.java)
+  }
+
+  @Singleton
+  private fun getClientWithInterceptors(): OkHttpClient{
+    return OkHttpClient.Builder()
+      .addInterceptor(AuthInterceptor(/*TODO("add token provider")*/))
+      .build()
   }
 
   @Provides
