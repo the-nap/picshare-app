39fad0f02b81aa6b9dbb6636935d57539289accf
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-08-19 16:00:52 +0200

    fix: minor

--- app/src/main/java/com/picshare/app/data/model/PostModel.kt
+++ app/src/main/java/com/picshare/app/data/model/PostModel.kt
@@ -1,3 +1,9 @@
 package com.picshare.app.data.model
 
-data class PostModel(val id: String, val userId: String, val description: String, val tags: String, val likesNumber: Number)
+data class PostModel(
+  val id: String,
+  val userId: String,
+  val description: String?,
+  val tags: String,
+  val likesNumber: Number
+)
--- app/src/main/java/com/picshare/app/data/model/UserModel.kt
+++ app/src/main/java/com/picshare/app/data/model/UserModel.kt
@@ -3,7 +3,7 @@ package com.picshare.app.data.model
 data class UserModel(
   val id: String,
   val username: String,
-  val bio: String,
+  val bio: String?,
   val followersCount: Number,
   val followedCount: Number
 )
