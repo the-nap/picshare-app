b7c19090b6f377f85a57b4298fefe340f4af21f9
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-09-08 23:32:41 +0200

    feat: add detached notification after upload

--- app/src/main/java/com/picshare/app/ui/post/upload/UploadService.kt
+++ app/src/main/java/com/picshare/app/ui/post/upload/UploadService.kt
@@ -1,5 +1,7 @@
 package com.picshare.app.ui.post.upload
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.net.Uri
@@ -7,7 +9,9 @@ import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.picshare.app.R
 import com.picshare.app.api.network.Util.NetworkResult
 import com.picshare.app.data.model.PostModel
@@ -40,6 +44,7 @@ class UploadService: Service() {
     return super.onStartCommand(intent, flags, startId)
   }
 
+  @SuppressLint("MissingPermission")
   @RequiresApi(Build.VERSION_CODES.TIRAMISU)
   private fun startUpload(intent: Intent) {
     val notification = NotificationCompat.Builder(this, "upload_channel")
@@ -49,30 +54,48 @@ class UploadService: Service() {
       .build()
     startForeground(1, notification)
 
-    val uri = intent.getParcelableExtra("uri", Uri::class.java) ?: return stopSelfCleanly()
+    val uri = intent.getParcelableExtra("uri", Uri::class.java) ?: return stopError()
     val sizeBytes = intent.getLongExtra("sizeBytes", -1L)
-    val post = intent.getParcelableExtra("post", PostModel::class.java) ?: return stopSelfCleanly()
+    val post = intent.getParcelableExtra("post", PostModel::class.java) ?: return stopError()
 
     serviceScope.launch ( CoroutineExceptionHandler{_, throwable ->
       Log.e(this.toString(), "unhandled error", throwable)
-      stopSelfCleanly()
+      stopError()
     }) {
       when(val result = postRepository.upload(uri, sizeBytes, post)){
         is NetworkResult.Success -> {
           UploadStatusBus.update(UploadStatus.Success)
+          stopSuccess()
         }
         is NetworkResult.Error -> {
           Log.e(this.toString(), "error: ${result.message}")
           UploadStatusBus.update(UploadStatus.Error(result.message))
+          stopError()
         }
       }
-      stopSelfCleanly()
     }
   }
 
-  private fun stopSelfCleanly() {
-    stopForeground(STOP_FOREGROUND_REMOVE)
+  @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+  private fun showResultNotification(title: String, text: String){
+    val notification = NotificationCompat.Builder(this, "upload_channel")
+      .setSmallIcon(R.drawable.logo_complete)
+      .setContentTitle(title)
+      .setContentText(text)
+      .setAutoCancel(true)
+      .build()
+
+    NotificationManagerCompat.from(this).notify(1, notification)
+    stopForeground(STOP_FOREGROUND_DETACH)
     stopSelf()
+  }
+  @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+  private fun stopSuccess() {
+    showResultNotification("Upload completed", "Your picture is now online!")
+  }
+  @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+  private fun stopError() {
+    showResultNotification("Upload failed", "We had problems during upload")
   }
   enum class Actions {
     UPLOAD
