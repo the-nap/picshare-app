06e1589bb5ebbea5d5f2230fa8689cba932f1dbd
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-09-08 23:14:31 +0200

    feat: add parcel to send object in intent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.serialization)
+    id("kotlin-parcelize")
 }
 
 android {
--- app/src/main/java/com/picshare/app/data/model/PostModel.kt
+++ app/src/main/java/com/picshare/app/data/model/PostModel.kt
@@ -1,9 +1,13 @@
 package com.picshare.app.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class PostModel(
   val id: String,
   val userId: String,
   val description: String,
   val tags: String,
   val likesNumber: Number
-)
+): Parcelable
